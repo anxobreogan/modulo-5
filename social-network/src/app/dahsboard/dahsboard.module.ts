1507bef4710f874b825ece65a50d34ad86d09b22
[Anxo] <[anxobreogan@hotmail.com]>
2019-03-15 21:25:36 +0100

    crear modulos

---
+++ social-network/src/app/dahsboard/dahsboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DahsboardRoutingModule } from './dahsboard-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DahsboardRoutingModule
+  ]
+})
+export class DahsboardModule { }
